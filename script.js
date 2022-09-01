2af251448b6d4677382d72ba72e302a80d0e956c
jo-well29 <rjowel@gmail.com>
2022-09-01 18:57:59 -0400

    feat: problem 3 solved

--- script.js
+++ script.js
@@ -23,15 +23,15 @@ const logUsers = () => {
     const getUsers = fetch(`https://jsonplaceholder.typicode.com/users/`)
 
     getUsers
-            .then(response => {
-                return response.json();
-            })
-                .then(data => {
-                    let userName = data.map(function(user){
-                        return user.name
-                    })
-                    console.log(userName)
-                })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            let userName = data.map(function (user) {
+                return user.name
+            }).join(", ")
+            console.log(userName)
+        })
 }
 
 logUsers()
@@ -40,19 +40,55 @@ logUsers()
 
 /*
 const logUsers = async function () {
-	const usersReq = await fetch("https://jsonplaceholder.typicode.com/users/")
-	const json = await usersReq.json()
-	const users = json.map(
-		function (user) {
-			return user.name
-		}).join(", ")
-	console.log(users)
+    const getUsers = await fetch("https://jsonplaceholder.typicode.com/users/")
+    const json = await getUsers.json()
+    const users = json.map(
+        function (user) {
+            return user.name
+        }).join(", ")
+    console.log(users)
 }
 
 logUsers()
 */
 
 // Problem 3
+/*
+const getBizUsers = () => {
+    const getBiz = fetch(`https://jsonplaceholder.typicode.com/users`);
 
+    getBiz
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            let findBiz = data.filter(function (user) {
+                if (user.email.includes('biz')) {
+                    return user
+                }
+            })
+            console.log(findBiz)
+        })
 
+}
+
+getBizUsers()
+*/
+
+//async & await
+
+const getBizUsers = async function() {
+    const getBiz = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    const json = await getBiz.json();
+    const findBiz = json.filter(function (user) {
+        if (user.email.includes('biz')) {
+            return user
+        }
+    })
+    console.log(findBiz)
+}
+
+getBizUsers()
+
+// Problem 4
 
