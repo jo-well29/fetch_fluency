bd8533099acf6509bf44f0fddac2bbdda3e157e1
jo-well29 <rjowel@gmail.com>
2022-09-01 15:44:05 -0400

    feat: problem 2

--- script.js
+++ script.js
@@ -17,4 +17,34 @@ const logPost = function (postId) {
 
 logPost(5)
 
+//Problem 2
+
+// const logUsers = () => {
+//     const getUsers = fetch(`https://jsonplaceholder.typicode.com/users/`)
+
+//     getUsers
+//             .then(response => {
+//                 return response.json();
+//             })
+//             .then(data.map(user => {
+//                 console.log(user.user)
+//             }))
+// }
+
+// logUsers()
+
+const logUsers = async function () {
+	const usersReq = await fetch("https://jsonplaceholder.typicode.com/users/")
+	const json = await usersReq.json()
+	const users = json.map(
+		function (user) {
+			return user.name
+		}).join(", ")
+	console.log(users)
+}
+
+logUsers()
+
+
+
 
