9eac3f5ca6f6516f395e281cefe7cb20385bd629
jo-well29 <rjowel@gmail.com>
2022-09-01 15:27:15 -0400

    feat: answered question 1

---
+++ script.js
@@ -0,0 +1,20 @@
+//Problem 1
+
+const logPost = function (postId) {
+    const getId = fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+
+    getId
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(
+                `Title: ${data.title}
+                Post: ${data.body}`
+            )
+        })
+}
+
+logPost(5)
+
+
